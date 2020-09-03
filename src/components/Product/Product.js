c4ef15465c4fa5b97421fa8aa23a9598c3addaa0
Raian-Siddique <raian0197@gmail.com>
2020-09-03 16:07:07 +0600

    pointer added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import "./product.css";
 
 const Product = (props) => {
@@ -21,6 +23,13 @@ const Product = (props) => {
         <p>
           <small>only{stock} left in stock- order soon</small>
         </p>
+        <button
+          className="main-button"
+          onClick={() => props.handleAddProduct(props.product)}
+        >
+          {" "}
+          <FontAwesomeIcon icon={faShoppingCart} /> add to cart
+        </button>
       </div>
     </div>
   );
