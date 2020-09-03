a6f47459e721ca6dd3cfcba284bfa5dc6e288fc1
Raian-Siddique <raian0197@gmail.com>
2020-09-03 13:44:07 +0600

    dynamic calling data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
-import logo from "./logo.svg";
+
 import "./App.css";
+import Header from "./components/Header/Header";
+import Shop from "./components/Shop/Shop";
+import Product from "./components/Product/Product";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header></Header>
+      <Shop></Shop>
+    </div>
+  );
 }
 
 export default App;
